a794be62c7c4ac5cdbca96d202ea98986083b0cc
Keerthi Vasan <155253910+keerthivasan2211@users.noreply.github.com>
2025-12-04 23:55:29 +0530

    Update resume file path to new PDF

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HERO_CONTENT } from '../constants';
 import { motion } from 'framer-motion';
 import profilePic from '../assets/keerthivasan.jpg';
-import resume from '../assets/Keerthivasan.pdf';
+import resume from '../assets/Keerthivasan_Resume.pdf';
 
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
